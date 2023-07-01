bb40e332aa4f03ad36f7524b239c0fc008a58fe6
Matheus-Mapg <mapg.goes@gmail.com>
2023-06-30 21:59:20 -0300

    ✨ finalizada a tela da questão 2

---
+++ ts/question-two.js
@@ -0,0 +1,36 @@
+async function questionTwo(file) {
+
+    const formData = new FormData()
+
+    formData.append('file', file)
+
+    const responseAjax = await fetch('http://localhost:3000/decipher-commands', { method: 'POST', body: formData })
+
+    const responseJson = await responseAjax.json()
+
+    const addres = responseJson.data
+
+    const div = document.createElement('div')
+
+    div.className = 'text'
+
+    const p = document.createElement('p')
+
+    p.innerHTML = `Endereço encontrado: ${addres}`
+
+    div.appendChild(p)
+
+    document.body.appendChild(div)
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const execute = document.getElementById('question-two')
+
+    execute.onchange = function () {
+        questionTwo(execute.files[0]).then(() => {
+        })
+
+
+    };
+});
